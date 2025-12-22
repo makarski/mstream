b7457819f1997392c7d98242b5df6f5903fcd2ce
Peter Caron <peter.caron@3tsoftwarelabs.com>
2025-12-22 21:42:12 +0100

    Remove accidentally added files

--- create_emulator_res.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 10 && \
-   curl -s -X PUT "http://localhost:8085/v1/projects/test-project/topics/test-topic" && \
-   curl -s -X PUT "http://localhost:8085/v1/projects/test-project/subscriptions/test-subscription" \
-     -H "Content-Type: application/json" \
-     -d '{"topic":"projects/test-project/topics/test-topic"}' && \
-   echo -e "\nEmulator resources created"
