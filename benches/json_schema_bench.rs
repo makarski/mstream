66261024f2126ce10e348541b0868bd44c89cdc0
Simon Makarski <code@makarski.dev>
2026-01-28 17:31:33 +0100

    Update json bson conversion benchmark

--- benches/json_schema_bench.rs
+++ benches/json_schema_bench.rs
@@ -1,4 +1,6 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
 use mongodb::bson::{Document, doc};
 use serde_json::json;
 
